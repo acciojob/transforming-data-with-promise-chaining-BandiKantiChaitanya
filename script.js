6d84445db0f0159a28df1774489de87ee90ecd32
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-28 16:20:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,86 @@
 //your JS code here. If required.
+let ip=document.getElementById('ip')
+let btn=document.getElementById('btn')
+let output=document.getElementById('output')
+
+
+btn.addEventListener('click',()=>{
+	let ipval=ip.value
+	transformNum(ipval)
+	// updateUi(ipval)
+})
+
+function updateUi(num){
+	output.innerHTML+=`Result: ${num} <br/>`
+	
+}
+
+function printNum(ipval){
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			updateUi(ipval)
+			resolve(ipval)
+		},2000)
+	})
+}
+
+function mulNum(ipval) {
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			ipval=ipval*2
+			updateUi(ipval)
+			resolve(ipval)
+		},1000)
+	})
+}
+
+function subNum(ipval) {
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			ipval=ipval-3
+			updateUi(ipval)
+			resolve(ipval)
+		},1000)
+	})
+}
+
+function divNum(ipval) {
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			ipval=ipval/2
+			updateUi(ipval)
+			resolve(ipval)
+		},1000)
+	})
+}
+
+
+
+function AddNum(ipval) {
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			ipval=ipval+10
+			updateUi(ipval)
+			resolve(ipval)
+		},1000)
+	})
+}
+
+function transformNum(ipval){
+	printNum(ipval)
+	.then(num=>{
+		return mulNum(num)
+	})
+	.then(num=>{
+		return subNum(num)
+	})
+	.then(num=>{
+		return divNum(num)
+	})
+	.then(num=>{
+		return AddNum(num)
+	})
+	.catch(err=>{
+		console.log(err)
+	})
+}
